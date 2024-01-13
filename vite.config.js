05803a37aed13e8533e40272122455df4d110aa7
Dimitar Chishirov <dimitarchishirov@gmail.com>
2024-01-13 14:54:16 +0100

    add-vite-config-js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Ändere den Port deines Entwicklungsservers
+    open: true, // Öffne einen Browser beim Start des Servers
+  },
+  build: {
+    outDir: 'dist', // Ändere das Ausgabe-Verzeichnis für den Build
+  },
 })
